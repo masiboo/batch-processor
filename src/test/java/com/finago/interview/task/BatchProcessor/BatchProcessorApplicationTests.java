40937c8b27ccff0b513ac3282d7a68fa6aeb51a7
Masum Islam <madsum@gmail.com>
2021-08-24 11:27:36 +0300

    Add generated spring boot project

---
+++ src/test/java/com/finago/interview/task/BatchProcessor/BatchProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finago.interview.task.BatchProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
